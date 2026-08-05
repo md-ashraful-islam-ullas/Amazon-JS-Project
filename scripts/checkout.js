3803607b617789cb6f3f653eeebc9eb55b89dee1
md-ashraful-islam-ullas <ashraful.ullas@gmail.com>
2026-08-05 11:20:11 +0600

    feat: Going to start from 13:30 tomorrow

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -36,7 +36,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
